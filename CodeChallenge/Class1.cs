3ed4d37cddaa8ae3fba4cc402d78d18376774fd7
vinicius <viniciusartur@protonmail.com>
2025-01-22 15:00:15 +0800

    Added keypad for numeric keys

--- CodeChallenge/Class1.cs
+++ CodeChallenge/Class1.cs
@@ -3,6 +3,18 @@
 public static class Class1
 {
     public static String OldPhonePad(string input) {
+        var keypad = new Dictionary<string, string[]>()
+        {
+            { "2", ["A", "B", "C"] },
+            { "3", ["D", "E", "F"] },
+            { "4", ["G", "H", "I"] },
+            { "5", ["J", "K", "L"] },
+            { "6", ["M", "N", "O"] },
+            { "7", ["P", "Q", "R", "S"] },
+            { "8", ["T", "U", "V"] },
+            { "9", ["W", "X", "Y", "Z"] }
+        };
+
         return "E";
     }
 }
